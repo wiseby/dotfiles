13929f77bd6b2dce8d610408edc9c602cf028e1e
Lucas Wiseby <l.wiseby@gmail.com>
2025-01-06 07:58:44 +0100

    add install script wip

---
+++ install.sh
@@ -0,0 +1,7 @@
+pacman -Syu
+pacman -S bash-completion
+
+bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)"
+
+curl -s https://ohmyposh.dev/install.sh | bash -s
+
